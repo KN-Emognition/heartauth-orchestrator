82aa64af3ca72714d6a51116b90dc3085d149cee
Karol Zajac <karolzajac.0407@gmail.com>
2025-08-17 20:44:29 +0200

    feat: add fcm integration & security measures

--- src/main/java/zpi/heartAuth/orchestrator/pairDevice/controller/PairDeviceController.java
+++ src/main/java/zpi/heartAuth/orchestrator/pairDevice/controller/PairDeviceController.java
@@ -4,16 +4,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.RestController;
 import zpi.heartAuth.orchestrator.api.PairingApi;
-import zpi.heartAuth.orchestrator.model.PairingInitResponse;
+import zpi.heartAuth.orchestrator.model.*;
+
+import java.util.UUID;
 
 @RestController
 public class PairDeviceController implements PairingApi {
 
     @PreAuthorize("hasAuthority('keycloak')")
     @Override
-    public ResponseEntity<PairingInitResponse> pairExchangeInitPost(
-            @org.springframework.web.bind.annotation.RequestBody
-            zpi.heartAuth.orchestrator.model.PairingInitRequest pairingInitRequest) {
+    public ResponseEntity<PairingInitResponse> pairExchangeInitPost(PairingInitRequest pairingInitRequest) {
         return ResponseEntity.ok(new PairingInitResponse());
+    }
+
+    @PreAuthorize("hasAuthority('keycloak')")
+    @Override
+    public ResponseEntity<PairingStatusResponse> pairStatusJtiGet(UUID jti, String xKCSession) {
+        return ResponseEntity.ok(new PairingStatusResponse());
+    }
+
+    public ResponseEntity<PairingConfirmResponse> pairExchangeConfirmPost(PairingConfirmRequest pairingConfirmRequest) {
+        return ResponseEntity.ok(new PairingConfirmResponse());
     }
 }
