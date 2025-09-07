293f429bdad55740884abd5ec7a72cea24300f4f
Karol Zajac <karolzajac.0407@gmail.com>
2025-09-07 21:43:02 +0200

    remove redundant exception

--- src/main/java/knemognition/heartauth/orchestrator/internal/config/errorhandling/exception/ApiKeyException.java
+++
@@ -1,7 +0,0 @@
-package knemognition.heartauth.orchestrator.internal.config.errorhandling.exception;
-
-public class ApiKeyException extends RuntimeException {
-    public ApiKeyException(String message) {
-        super(message);
-    }
-}
--- src/main/java/knemognition/heartauth/orchestrator/internal/config/errorhandling/handler/InternalExceptionHandler.java
+++ src/main/java/knemognition/heartauth/orchestrator/internal/config/errorhandling/handler/InternalExceptionHandler.java
@@ -1,7 +1,6 @@
 package knemognition.heartauth.orchestrator.internal.config.errorhandling.handler;
 
 import jakarta.servlet.http.HttpServletRequest;
-import knemognition.heartauth.orchestrator.internal.config.errorhandling.exception.ApiKeyException;
 import knemognition.heartauth.orchestrator.internal.config.errorhandling.exception.FirebaseSendException;
 import knemognition.heartauth.orchestrator.internal.config.errorhandling.exception.NoActiveDeviceException;
 import knemognition.heartauth.orchestrator.shared.config.errorhandling.StatusServiceException;
@@ -32,8 +31,4 @@ public class InternalExceptionHandler {
         return problem(HttpStatus.BAD_REQUEST, "Status operation for given flow failed.", req, ex);
     }
 
-    @ExceptionHandler(ApiKeyException.class)
-    public ProblemDetail handleApiKey(Exception ex, HttpServletRequest req) {
-        return problem(HttpStatus.BAD_REQUEST, "Failed to validate with API Key.", req, ex);
-    }
 }
