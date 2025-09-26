be0c8af4c8465bc0051dc8afe9d45ca829b79986
Karol Zajac <karolzajac.0407@gmail.com>
2025-09-26 23:10:14 +0200

    fix: mapping issue

--- src/main/java/knemognition/heartauth/orchestrator/shared/gateways/persistence/jpa/entity/DeviceCredentialEntity.java
+++ src/main/java/knemognition/heartauth/orchestrator/shared/gateways/persistence/jpa/entity/DeviceCredentialEntity.java
@@ -4,6 +4,7 @@ import jakarta.persistence.*;
 import knemognition.heartauth.orchestrator.external.model.Platform;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
+import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.JdbcTypeCode;
@@ -17,6 +18,7 @@ import java.util.UUID;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
+@Data
 @Table(name = "device_credential")
 public class DeviceCredentialEntity {
 
