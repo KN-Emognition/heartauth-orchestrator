a099e0c343f423c578a4cd873f00bc5397754b0e
Karol Zajac <karolzajac.0407@gmail.com>
2025-11-22 22:44:05 +0100

    feat: add tests

--- src/test/java/knemognition/heartauth/orchestrator/ModulithTest.java
+++ src/test/java/knemognition/heartauth/orchestrator/ModulithTest.java
@@ -5,18 +5,16 @@ import org.junit.jupiter.api.Test;
 import org.springframework.modulith.core.ApplicationModules;
 import org.springframework.modulith.docs.Documenter;
 
-class DocumentationTests {
+class ModulithTest {
 
     ApplicationModules modules = ApplicationModules.of(OrchestratorApplication.class);
 
     @Test
     void writeDocumentationSnippets() {
-
         new Documenter(modules)
                 .writeModulesAsPlantUml()
                 .writeIndividualModulesAsPlantUml()
                 .writeDocumentation();
 
     }
-
 }
--- src/test/java/test/config/HeartauthSpringBootTest.java
+++ src/test/java/test/config/HeartauthSpringBootTest.java
@@ -1,15 +1,19 @@
 package test.config;
 
-
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest(properties = {
         "spring.flyway.enabled=false",
         "spring.datasource.url=jdbc:h2:mem:orchestrator;MODE=PostgreSQL;DB_CLOSE_DELAY=-1",
         "spring.datasource.driver-class-name=org.h2.Driver",
-        "spring.datasource.username=sa",
-        "spring.datasource.password=",
-        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect"
+        "spring.datasource.username=user",
+        "spring.datasource.password=password",
+        "spring.jpa.database-platform=org.hibernate.dialect.H2Dialect",
+        "spring.jpa.hibernate.ddl-auto=none",
+        "spring.sql.init.mode=always",
+        "spring.sql.init.schema-locations=classpath:test-schema.sql",
+        "admin.api.key=test-admin-key",
+        "spring.kafka.streams.auto-startup=false"
 })
-public class HeartauthSpringBootTest {
+public abstract class HeartauthSpringBootTest {
 }
